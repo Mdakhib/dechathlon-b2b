0ab9abb3dc744f7d71a20b45e76a10afe55b7598
saqlain1345 <saqlainkhan1345@gmail.com>
2021-10-05 16:49:23 +0530

    manufature and nature filter added

--- src/assets/data/filterData.js
+++ src/assets/data/filterData.js
@@ -5,42 +5,254 @@ export const sportsFilter = [
   },
   {
     id: 2,
-    name: "HIP-HOP",
+    name: "WAACKING",
   },
   {
     id: 3,
-    name: "HIP-HOP",
+    name: "TRAMPOLING",
   },
   {
     id: 4,
-    name: "HIP-HOP",
+    name: "REGARTA",
   },
   {
     id: 5,
-    name: "HIP-HOP",
+    name: "OUTDOOR RIDING",
+  },
+  {
+    id: 6,
+    name: "HANDLE FISHING",
+  },
+  {
+    id: 7,
+    name: "YACHT SAILING",
+  },
+  {
+    id: 8,
+    name: "DINGHY CATAMARAN SAILING",
+  },
+  {
+    id: 9,
+    name: "WAVEBOARD",
+  },
+  {
+    id: 10,
+    name: "CRICKET",
+  },
+  {
+    id: 11,
+    name: "LACROSSE",
+  },
+  {
+    id: 12,
+    name: "SOFTBALL",
+  },
+  {
+    id: 13,
+    name: "SQUASH",
+  },
+  {
+    id: 14,
+    name: "TOUCH RUBGY",
+  },
+  {
+    id: 15,
+    name: "CROSS COUNTRY SKATING",
+  },
+  {
+    id: 16,
+    name: "SHORE FISHING",
+  },
+  {
+    id: 17,
+    name: "JUDO",
+  },
+  {
+    id: 18,
+    name: "WINDSURF WAVE",
+  },
+  {
+    id: 19,
+    name: "BALLET",
+  },
+  {
+    id: 20,
+    name: "EDUCATIONAL SPORTS",
+  },
+
+];
+
+
+export const manufatureFilter = [
+  {
+    id: 1,
+    name: "Forclaz",
+  },
+  {
+    id: 2,
+    name: "Quechua",
+  },
+  {
+    id: 3,
+    name: "Kipsta",
   },
   {
     id: 4,
-    name: "HIP-HOP",
+    name: "Domyos",
   },
   {
     id: 5,
-    name: "HIP-HOP",
+    name: "Kripun",
+  },
+  {
+    id: 6,
+    name: "Inesis",
+  },
+  {
+    id: 7,
+    name: "Outshock",
+  },
+  {
+    id: 8,
+    name: "Kalenji",
+  },
+  {
+    id: 9,
+    name: "Artengo",
+  },
+  {
+    id: 10,
+    name: "Perfly",
+  },
+  {
+    id: 11,
+    name: "Solognac",
+  },
+  {
+    id: 12,
+    name: "Evadict",
+  },
+  {
+    id: 13,
+    name: "Nabaji",
+  },
+  {
+    id: 14,
+    name: "Riverside",
+  },
+  {
+    id: 15,
+    name: "Itiwit",
+  },
+  {
+    id: 16,
+    name: "Oxelo",
+  },
+  {
+    id: 17,
+    name: "Simond",
+  },
+  {
+    id: 18,
+    name: "Btwin",
+  },
+  {
+    id: 19,
+    name: "Elops",
+  },
+  {
+    id: 20,
+    name: "Korok",
+  },
+];
+
+
+export const natureFilter = [
+  {
+    id: 1,
+    name: "Backpack",
+  },
+  {
+    id: 2,
+    name: "Sport bag",
+  },
+  {
+    id: 3,
+    name: "Sleeping bag",
   },
   {
     id: 4,
-    name: "HIP-HOP",
+    name: "Storage cover",
   },
   {
     id: 5,
-    name: "HIP-HOP",
+    name: "Water bottle carrier belt",
   },
   {
-    id: 4,
-    name: "HIP-HOP",
+    id: 6,
+    name: "Carry bag",
   },
   {
-    id: 5,
-    name: "HIP-HOP",
+    id: 7,
+    name: "Punch bag",
+  },
+  {
+    id: 8,
+    name: "Golf bag",
+  },
+  {
+    id: 9,
+    name: "Mattress",
+  },
+  {
+    id: 10,
+    name: "Water bag",
+  },
+  {
+    id: 11,
+    name: "Waterproof bag",
+  },
+  {
+    id: 12,
+    name: "Frame bag",
+  },
+  {
+    id: 13,
+    name: "Padlock",
+  },
+  {
+    id: 14,
+    name: "Shoe bag",
+  },
+  {
+    id: 15,
+    name: "Bag",
+  },
+  {
+    id: 16,
+    name: "Pillow",
+  },
+  {
+    id: 17,
+    name: "Pouch",
+  },
+  {
+    id: 18,
+    name: "Saddle bag",
+  },
+  {
+    id: 19,
+    name: "Travelling bag",
+  },
+  {
+    id: 20,
+    name: "Trolley",
+  },
+];
+
+export const colorFilter = [
+  {
+    id: 1,
+    bg: 'red'
   },
 ];
