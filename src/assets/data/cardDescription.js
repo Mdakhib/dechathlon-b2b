bde5f6d2bee125fcdf7a406dcf5b810c06d37004
saqlain1345 <saqlainkhan1345@gmail.com>
2021-10-05 10:21:46 +0530

    assets with data and images added

---
+++ src/assets/data/cardDescription.js
@@ -0,0 +1,108 @@
+import Bag1 from "../images/bag1.jpg";
+import Bag2 from "../images/bag2.jpg";
+import Bag3 from "../images/bag3.jpg";
+import Bag4 from "../images/bag4.jpg";
+import Bag5 from "../images/bag5.jpg";
+import Bag6 from "../images/bag6.jpg";
+import Bag7 from "../images/bag7.jpg";
+import Bag8 from "../images/bag8.jpg";
+import Bag9 from "../images/bag9.jpg";
+import Bag10 from "../images/bag10.jpg";
+
+
+
+const cardDescription = [
+    {
+        id: 1,
+        img: Bag1,
+        price: "1365.00",
+        title: "Wildcraft Ruksack for HIKING/CLIMBING/MOUNTAINEERING/SKIING/SNOWBOARDING",
+        capacity: "45 Ltrs",
+        color: "Grey and Yellow",
+
+    },
+    {
+        id: 2,
+        img: Bag2,
+        price: "1436.00",
+        title: "TRAWOC Travel Backpack Hiking Trekking Rucksack Bag",
+        capacity: "60 Ltrs",
+        color: "Black",
+
+    },
+    {
+        id: 3,
+        img: Bag3,
+        price: "2698.00",
+        title: "wildcraft Rucksack for SKIING/SNOWBOARDING/HIKING",
+        capacity: "60 Ltrs",
+        color: "Black",
+
+    },
+    {
+        id: 4,
+        img: Bag4,
+        price: "5390.00",
+        title: "Wildcraft Backpack for SKIING/SNOWBOARDING/HIKING",
+        capacity: "65 Ltrs",
+        color: "Green",
+
+    },
+    {
+        id: 5,
+        img: Bag5,
+        price: "3099.00",
+        title: "AmazonBasics Internal Frame (Hardback) Hiking Backpack with Raincover",
+        capacity: "55 Ltrs",
+        color: "Black",
+
+    },
+    {
+        id: 6,
+        img: Bag6,
+        price: "1690.00",
+        title: "Aristocrat Rucksack for HIKING (RUCHIKEGRY)",
+        capacity: "45 Ltrs",
+        color: "Grey",
+
+    },
+    {
+        id: 7,
+        img: Bag7,
+        price: "1690.00",
+        title: "Backpack for Outdoor Sport Hiking Trekking Bag Camping Rucksack",
+        capacity: "65 Ltrs",
+        color: "Yellow",
+
+    },
+    {
+        id: 8,
+        img: Bag8,
+        price: "4769.00",
+        title: "Deuter Ocean and Midnight Hiking Backpacks",
+        capacity: "24 Ltrs",
+        color: "Ocean Blue",
+
+    },
+    {
+        id: 9,
+        img: Bag9,
+        price: "2969.00",
+        title: "Tripole Colonel Rucksack + Detachable Day Pack",
+        capacity: "80 Ltrs",
+        color: "Army Green",
+
+    },
+    {
+        id: 10,
+        img: Bag10,
+        price: "4997.00",
+        title: "Wildcraft Red Hiking Backpack Camping Rucksack",
+        capacity: "65 Ltrs",
+        color: "Orange",
+
+    },
+];
+
+export default cardDescription;
+
